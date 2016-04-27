51f422253f160f5bc5d229975692e294b3f62803
Markus Kainu <markuskainu@gmail.com>
2016-04-27 16:26:03 +0300

    little of bit structure

--- sync.sh
+++ sync.sh
@@ -1,2 +1,4 @@
 #
-rsync -arv --exclude=local_data --exclude=figure --exclude=byroo --exclude=run.R --exclude='2015_jani.Rproj' --exclude=.Rhistory --exclude=.git --exclude=.gitignore --exclude=.Rproj.user ~/btsync/mk/workspace/russia/huippari2016/aisurvey_web/_site/ muuankarski@kapsi.fi:sites/muuankarski.kapsi.fi/www/aisurvey/
+rsync -arv --exclude=data --exclude=site_libs --exclude=byroo --exclude=run.R --exclude='2015_jani.Rproj' --exclude=.Rhistory --exclude=.git --exclude=.gitignore --exclude=.Rproj.user ~/btsync/mk/workspace/russia/huippari2016/aisurvey_web/_site/ muuankarski@kapsi.fi:sites/muuankarski.kapsi.fi/www/aisurvey/
+
+# rsync -arv muuankarski@kapsi.fi:sites/muuankarski.kapsi.fi/www/aisurvey/data/ ~/btsync/mk/workspace/russia/huippari2016/aisurvey_web/data/
